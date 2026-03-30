41d4eb76dad91d9b48abfb6d3e8aa900b57e3b7c
Pitt Hammerit <ben.kurtz@gmx.net>
2026-03-31 00:05:49 +0530

    test: SimpleCtaSection button spacing inline style 160px

--- frontend/src/components/sections/SimpleCtaSection.jsx
+++ frontend/src/components/sections/SimpleCtaSection.jsx
@@ -41,7 +41,7 @@ export default function SimpleCtaSection({ debugMode = false }) {
           </p>
         </DebugLabel>
 
-        <div className="mt-[10rem]">
+        <div style={{ marginTop: '160px' }}>
           <DebugLabel type="button-text" debugMode={debugMode}>
             <CtaButton label={simpleCta.cta} variant="primary" className="py-4 px-12" />
           </DebugLabel>
